80ae42aaabff6298913f9ac300dad76f1cd256dd
SachiniU <99791531+SachiniU@users.noreply.github.com>
2022-03-06 15:58:47 +0530

    Add project files.

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using CustomerAPI.Models;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CustomerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        // ----- Get Customer ------
+        [HttpGet]
+        public JsonResult Get()
+        {
+            DataTable dtable = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("Myconnection");
+            SqlDataReader myReader;
+
+            using (SqlConnection mycon = new SqlConnection(sqlDataSource))
+            { 
+                mycon.Open();
+
+                SqlCommand myCom = new SqlCommand("GetCustomer_sp", mycon);
+                myCom.CommandType = CommandType.StoredProcedure;
+                myReader = myCom.ExecuteReader();
+                dtable.Load(myReader);
+                myReader.Close();
+                mycon.Close();
+                
+            }
+
+            return new JsonResult(dtable);
+        }
+
+
+        // ----- Post Customer -----
+        [HttpPost]
+        public JsonResult Post(CustomerModel cus)
+        {
+            string sqlDataSource = _config.GetConnectionString("Myconnection");
+
+            using (SqlConnection mycon = new SqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                SqlCommand myCom = new SqlCommand("InsertCustomer_sp", mycon);
+                myCom.CommandType = CommandType.StoredProcedure;
+                
+                myCom.Parameters.AddWithValue("@Customer_Name", cus.Customer_Name);
+                myCom.Parameters.AddWithValue("@Customer_Phone", cus.Customer_Phone);
+                myCom.Parameters.AddWithValue("@Customer_Address", cus.Customer_Address);
+                myCom.Parameters.AddWithValue("@Customer_Email", cus.Customer_Email);
+                   
+                myCom.ExecuteReader();
+
+                mycon.Close();
+                
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        //----- Customer Update -----
+        [HttpPut]
+        public JsonResult Put(CustomerModel cus)
+        {
+            string sqlDataSource = _config.GetConnectionString("Myconnection");
+            
+            using (SqlConnection mycon = new SqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                SqlCommand myCom = new SqlCommand("UpdateCustomer_sp", mycon);
+                myCom.CommandType = CommandType.StoredProcedure;
+
+                myCom.Parameters.AddWithValue("@Customer_Id", cus.Customer_Id);
+                myCom.Parameters.AddWithValue("@Customer_Name", cus.Customer_Name);
+                myCom.Parameters.AddWithValue("@Customer_Phone", cus.Customer_Phone);
+                myCom.Parameters.AddWithValue("@Customer_Address", cus.Customer_Address);
+                myCom.Parameters.AddWithValue("@Customer_Email", cus.Customer_Email);
+                    
+                myCom.ExecuteReader();
+                    
+                mycon.Close();
+                
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        // ----- Customer Delete -----
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string sqlDataSource = _config.GetConnectionString("Myconnection");
+            
+            using (SqlConnection mycon = new SqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                SqlCommand myCom = new SqlCommand("DeleteCustomer_sp", mycon);
+                myCom.CommandType = CommandType.StoredProcedure;
+
+                myCom.Parameters.AddWithValue("@Customer_Id", id);
+                
+                myCom.ExecuteReader();
+                
+                mycon.Close();
+
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+    }
+}
---
+++ CustomerAPI/Models/CustomerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerAPI.Models
+{
+    public class CustomerModel
+    {
+        public int Customer_Id { get; set; }
+        public string Customer_Name { get; set; }
+        public string Customer_Phone { get; set; }
+        public string Customer_Address { get; set; }
+        public string Customer_Email { get; set; }
+
+    }
+}
